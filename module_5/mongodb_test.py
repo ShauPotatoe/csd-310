e1d66a34edc1dec18ed0c062a25a15032c393383
ShauPotatoe <105703866+ShauPotatoe@users.noreply.github.com>
2023-07-04 23:02:05 -0400

    Add files via upload

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,27 @@
+""" 
+    Title: mongodb_test.py
+    Author: Professor Krasso
+    Date: 10 July 2020
+    Description: Test program for connecting to a 
+                 MongoDB Atlas cluster
+"""
+
+""" import statements """
+import ssl
+from pymongo import MongoClient
+
+# MongoDB connection string 
+url = "mongodb+srv://admin:admin@cluster0.6pwfrmu.mongodb.net/pytech?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
+
+# connect to the MongoDB cluster 
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+# show the connected collections 
+print("\n -- Pytech COllection List --")
+print(db.list_collection_names())
+
+# show an exit message
+input("\n\n  End of program, press any key to exit... ")
